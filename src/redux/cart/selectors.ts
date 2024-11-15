7faf7e879935df1073200bec180b6a9570ed1fd3
TheAnderson11 <andersonshtf3311@gmail.com>
2024-11-15 20:50:00 +0200

    Updates

--- src/redux/cart/selectors.ts
+++
@@ -1,6 +0,0 @@
-import { RootState } from "../store";
-
-export const cartSelectorFindItems = (id: string) => (state: RootState) =>
-    state.cart.items.find(obj => obj.id === id);
-
-export const cartSelector = (state: RootState) => state.cart;
--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import rewriteAll from 'vite-plugin-rewrite-all';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/pizza-shop/',
-  plugins: [react(), rewriteAll()],
-});
