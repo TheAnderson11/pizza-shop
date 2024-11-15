db29dda3c45eff5828d92c97ac6b682caaa58c8d
TheAnderson11 <andersonshtf3311@gmail.com>
2024-11-15 20:52:51 +0200

    button come back path

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -121,7 +121,7 @@ const Cart: FC = () => {
             </div>
             <div className="cart__bottom-buttons">
               <a
-                href="/"
+                href="/pizza-shop/"
                 className="button button--outline button--add go-back-btn"
               >
                 <svg
